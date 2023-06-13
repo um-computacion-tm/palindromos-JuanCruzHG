24e17c963166aa9a530431319a7a9238baa0540b
JuanCruzHG <juan.cruz.herrera44@gmail.com>
2023-06-13 11:28:21 -0300

    entrega de palindromo

---
+++ palindromo.py
@@ -0,0 +1,25 @@
+
+import unittest
+
+def palindrome (Word):
+    descendiente = len(Word) - 1
+    ascendiente = 0
+    palindromo = True
+    for letra in Word:
+       if Word[ascendiente].lower() != Word[descendiente].lower():
+           palindromo = False 
+       descendiente -= 1
+       ascendiente += 1
+       return palindromo 
+
+def palindrome_r (word):
+    word = word.replace(' ','').lower()
+    if len(word) < 2:
+        return True
+    if word[0] == word[-1]:
+        return palindrome_r (word[1:-1])
+    else:
+        return False
+
+if __name__ == '__main__':
+    unittest.main()
